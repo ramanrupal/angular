4b9b7fef917579a0aed75f1be9251c806676d8bb
devashish01 <malhotradevashish01@gmail.com>
2019-03-20 20:42:38 +0530

    added reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import { SignupComponent } from './signup/signup.component';
+
+let config = {
+  apiKey: "AIzaSyAyrogPXBePjQB2gcV-J7BaCWt3WpVikGg",
+  authDomain: "scribe-3cdef.firebaseapp.com",
+  databaseURL: "https://scribe-3cdef.firebaseio.com",
+  projectId: "scribe-3cdef",
+  storageBucket: "scribe-3cdef.appspot.com",
+  messagingSenderId: "1063973320921"
+};
+
+
+firebase.initializeApp(config);
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+   
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  myForm: FormGroup;
+
+  constructor(public fb: FormBuilder) {
+
+    this.myForm=this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['',[Validators.required ]],
+      email:['',[Validators.required ]],
+      password: ['',[Validators.required ,Validators.minLength(8)]],
+      confirmPassword: ['',[Validators.required ]]
+
+    }, {
+       validator: this.checkIfMatchingPasswords("password","confirmPassword")
+    })
+
+   }
+
+   checkIfMatchingPasswords(passwordKey: string, confirmPasswordKey: string){
+    return (group: FormGroup) => {
+      let password = group.controls[passwordKey];
+      let confirmPassword = group.controls[confirmPasswordKey];
+
+      if(password.value == confirmPassword.value) 
+      {
+        return ;
+      }
+      else{
+        confirmPassword.setErrors({
+          notEqualToPassword: true
+        })
+      }
+    }
+
+   }
+
+   onSubmit(signupform){
+     console.log(signupform.value);
+   }
+  ngOnInit() {
+  }
+
+}
