e1ef21e3d2f65179b7321b7c55974d5d80090fb7
devashish01 <malhotradevashish01@gmail.com>
2019-03-22 02:16:27 +0530

    my profilr button non functional

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor() { }
+  user: any = {};
+  message: string;
+
+  constructor() {
+    this.getProfile();
+  }
 
   ngOnInit() {
+  }
+
+  getProfile(){
+
+    let userId = firebase.auth().currentUser.uid;
+
+    firebase.firestore().collection("users").doc(userId).get().then((documentSnapshot) => {
+
+      this.user = documentSnapshot.data();
+      this.user.displayName = this.user.firstName + " " + this.user.lastName;
+      this.user.id = documentSnapshot.id;
+      console.log(this.user);
+
+    }).catch((error) => {
+      console.log(error);
+    })
+
+  }
+
+  update(){
+
+    this.message = "Updating Profile...";
+
+    firebase.auth().currentUser.updateProfile({
+      displayName: this.user.displayName, photoURL: this.user.photoUrl
+    }).then(() => {
+
+      let userId = firebase.auth().currentUser.uid;
+      firebase.firestore().collection("users").doc(userId).update({
+        first_name: this.user.displayName.split(' ')[0],
+        last_name: this.user.displayName.split(' ')[1],
+        hobbies: this.user.hobbies,
+        interests: this.user.interests,
+        bio: this.user.bio
+      }).then(() => {
+
+        this.message = "Profile Updated Successfully.";
+
+      }).catch((error) => {
+        console.log(error)
+      })
+
+
+    }).catch((error) => {
+      console.log(error)
+    })
+
   }
 
 }
